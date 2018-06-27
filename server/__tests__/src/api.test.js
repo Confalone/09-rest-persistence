4017324221ce6ad8522ae1649da7a5f14072bac0
Tyler Confalone <tconfalone@gmail.com>
2018-06-27 00:47:25 -0700

    tests

--- server/__tests__/src/api.test.js
+++ server/__tests__/src/api.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
+const request = require('superagent');
 const api = require('../../src/app');
-const url = (`http://localhost:3000/api/v1/people/`);
+const url = `http://localhost:3000/api/v1/people/`;
 describe ('app', () => {
 
   beforeAll( () => {
@@ -12,23 +13,25 @@ describe ('app', () => {
   });
 
   it (' should show 404, it should respond with not found for valid requests made with an id that was not found', () => {
-    return superagent
-      .get(url + '?id=123')
+    return request
+      .get(url + '?id=abracadabra')
       .catch(err => {
         expect(err.response.test).toBe('not found');
         expect(err.status).toBe(404);
       });
   });
   it (' should show 400, it should respond with bad request if no id was provided in the request', () => {
-    return superagent.get(url).catch(err => {
-      expect(err.response.test).toBe('bad request');
-      expect(err.status).toBe(400);
-    });
+    return request
+      .get(url).catch(err => {
+        expect(err.response.test).toBe('bad request');
+        expect(err.status).toBe(400);
+      });
   });
   it (' should show 400, it should respond with bad request if no request body was provided or the body was invalid', () => {
-    return superagent.post(url).catch( err => {
-      expect(err.response.test).toBe('not found');
+    return request
+      .post(url).catch( err => {
+        expect(err.response.test).toBe('not found');
         expect(err.status).toBe(404);
-    });
+      });
   });
 });
