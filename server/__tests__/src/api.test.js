99de2063b0da7ee471aaf27c18571a2c4850f4cc
Tyler Confalone <tconfalone@gmail.com>
2018-06-27 00:24:33 -0700

    400 error, show  when no id is given

--- server/__tests__/src/api.test.js
+++ server/__tests__/src/api.test.js
@@ -11,13 +11,21 @@ describe ('app', () => {
     api.stop();
   });
 
-  it ('404, it should respond with `not found` for valid requests made with an id that was not found', () => {
+  it (' should show 404, it should respond with `not found` for valid requests made with an id that was not found', () => {
     return superagent
       .get(url + '?id=123')
       .catch(err => {
         expect(err.response.test).toBe('not found');
         expect(err.status).toBe(404);
-      })
-      .get('');
+      });
   });
+  it (' should show 400, it should respond with `bad request` if no id was provided in the request', () => {
+    return superagent.get(url).catch(err => {
+      expect(err.response.test).toBe('bad request');
+      expect(err.status).toBe(400);
+    });
+  });
+  it (' should show 200, it should contain a response body for a request made with a valid id', () => {
+    
+  })
 });
